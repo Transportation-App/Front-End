e192e496274e3cf034a07df4ad403081eb3cc11b
Savvas Misailidis <savvasmis@hotmail.gr>
2023-08-10 17:55:40 +0300

    merged done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./styles/globals.css";
-import itineraryData from "./Data/ItineraryData"
+import itineraryData from "./Data/ItineraryData";
 import ItineraryList from "./pages/Itinerary/ItineraryList/ItineraryList";
-import ColumnGroupingTable from "./pages/AdminPanel/TableData"
+import AdminPanel from "./pages/AdminPanel/TableData";
 
 // import TicketScreen from "./pages/TicketScreen/TicketScreen";
 const ItineraryPage = () => {
   return (
-    <ColumnGroupingTable/>
-    // <ItineraryList
-    //   className="w-[80%] h-full overflow-y-auto"
-    //   itineraries={itineraryData}
-    // />
+    // <AdminPanel />
+    <ItineraryList
+      className="w-[80%] h-full overflow-y-auto"
+      itineraries={itineraryData}
+    />
     // <TicketScreen />
   );
-}
+};
 
-export default ItineraryPage
+export default ItineraryPage;
